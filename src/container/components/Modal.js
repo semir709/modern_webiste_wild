48edefa2982a245802c4b23dde620eb313099130
semir709 <selmansemir555@gmail.com>
2024-11-27 10:04:16 +0100

    added the open animation modal

--- src/container/components/Modal.js
+++ src/container/components/Modal.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useLocomotiveScroll } from "react-locomotive-scroll";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { IoMdClose } from "react-icons/io";
 
 const Modal = () => {
   const [position, setPosition] = useState(0);
@@ -19,12 +21,32 @@ const Modal = () => {
     };
   }, [scroll]);
   return (
-    <div
+    <motion.div
       style={{ top: position }}
-      className="fixed z-50 flex h-screen w-full justify-end bg-black opacity-40"
+      className="fixed z-50 flex h-screen w-full justify-end"
     >
-      <div className="bg-red-300 p-5">hello</div>
-    </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.6 }}
+        className="absolute inset-0 bg-black"
+      />
+
+      <motion.div
+        initial={{ translateX: "100%" }}
+        animate={{ translateX: 0, transition: { duration: 0.4, delay: 0.3 } }}
+        className="relative flex w-[75%]"
+      >
+        <div className="p-5">
+          <button
+            onClick={() => navigate(-1)}
+            className="rounded-full bg-white p-1"
+          >
+            <IoMdClose fontSize={30} />
+          </button>
+        </div>
+        <div className="h-full w-full bg-white"></div>
+      </motion.div>
+    </motion.div>
   );
 };
 
