57dfb908e0486866477607681bf2bedc6791a45b
semir709 <selmansemir555@gmail.com>
2025-01-10 21:33:19 +0100

    fix scroll top on url change

--- src/App.js
+++ src/App.js
@@ -10,28 +10,34 @@ import {
   Blog,
 } from "./pages/index";
 import { ParticlesComponent } from "./container/components/index";
-import { Routes, Route } from "react-router-dom";
-import { useRef, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useLayoutEffect, useRef, useState } from "react";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import "locomotive-scroll/dist/locomotive-scroll.css";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
   const containerRef = useRef(null);
-  // const [textColorNav, setTextColorNav] = useState(0);
   const [darkMode, setDarkMode] = useState(false);
+
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: containerRef.current,
+      smooth: true,
+    });
+    scroll.scrollTo("top", {
+      offset: 0,
+    });
+
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, [location.pathname]);
 
   return (
     <>
-      {/* <div className="">
-        <div
-          style={{ transform: "matrix(1, 0, 0, 1, 0, ${value})" }}
-          className="h-screen w-full bg-red-300"
-        >
-          asds
-        </div>
-        <div className="relative bottom-0 mx-auto h-[200px] w-[200px] bg-blue-300"></div>
-      </div>
-      <div className="h-[1200px] w-full bg-orange-300">asd</div> */}
       <div
         className="fixed left-0 top-0 -z-10 h-full w-full"
         style={{
@@ -42,50 +48,48 @@ function App() {
         <ParticlesComponent darkMode={darkMode} />
       </div>
 
-      <LocomotiveScrollProvider
+      {/* <LocomotiveScrollProvider
         options={{
           smooth: true,
         }}
         watch={[]}
         containerRef={containerRef}
-      >
-        <main data-scroll-container ref={containerRef}>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <HomePage setDarkMode={setDarkMode} darkMode={darkMode} />
-              }
-            />
-            <Route path="/news" element={<NewsPage darkMode={darkMode} />} />
-            <Route
-              path="/careers"
-              element={
-                <CareersPage setDarkMode={setDarkMode} darkMode={darkMode} />
-              }
-            />
-            <Route
-              path="/thinking/*"
-              element={
-                <ThinkingPage setDarkMode={setDarkMode} darkMode={darkMode} />
-              }
-            />
-            <Route path="/contact" element={<Contact darkMode={darkMode} />} />
-            <Route path="/work" element={<WorkPage darkMode={darkMode} />} />
+      > */}
+      <main data-scroll-container ref={containerRef}>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage setDarkMode={setDarkMode} darkMode={darkMode} />}
+          />
+          <Route path="/news" element={<NewsPage darkMode={darkMode} />} />
+          <Route
+            path="/careers"
+            element={
+              <CareersPage setDarkMode={setDarkMode} darkMode={darkMode} />
+            }
+          />
+          <Route
+            path="/thinking/*"
+            element={
+              <ThinkingPage setDarkMode={setDarkMode} darkMode={darkMode} />
+            }
+          />
+          <Route path="/contact" element={<Contact darkMode={darkMode} />} />
+          <Route path="/work" element={<WorkPage darkMode={darkMode} />} />
 
-            <Route
-              path="/about/*"
-              element={
-                <AboutPage setDarkMode={setDarkMode} darkMode={darkMode} />
-              }
-            />
-            <Route
-              path="/blog/*"
-              element={<Blog setDarkMode={setDarkMode} darkMode={darkMode} />}
-            />
-          </Routes>
-        </main>
-      </LocomotiveScrollProvider>
+          <Route
+            path="/about/*"
+            element={
+              <AboutPage setDarkMode={setDarkMode} darkMode={darkMode} />
+            }
+          />
+          <Route
+            path="/blog/*"
+            element={<Blog setDarkMode={setDarkMode} darkMode={darkMode} />}
+          />
+        </Routes>
+      </main>
+      {/* </LocomotiveScrollProvider> */}
     </>
   );
 }
